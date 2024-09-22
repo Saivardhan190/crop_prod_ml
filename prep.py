36905296da4ea5e57e4325d11f07599e974d7fec
Saivardhan190 <153695133+Saivardhan190@users.noreply.github.com>
2024-09-22 22:33:12 +0530

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,18 +1,23 @@
 import streamlit as st
-import joblib
 import pandas as pd
+import joblib
 
+data = pd.read_csv("Crop Production data.csv")
+states = data['State_Name'].unique()
+seasons = data['Season'].unique()
+crops = data['Crop'].unique()
 model = joblib.load("production.pkl")
 feature_names = joblib.load("feature_names.pkl")
 scaler = joblib.load("scaler.pkl")
+
 st.title("Crop Production Prediction")
-state = st.text_input("State Name")
-district = st.text_input("District Name")
-season = st.text_input("Season")
-crop = st.text_input("Crop")
+state = st.selectbox("State_Name", states)
+filtered_districts = data[data['State_Name'] == state]['District_Name'].unique()
+district = st.selectbox("District_Name", filtered_districts)
+season = st.selectbox("Season", seasons)
+crop = st.selectbox("Crop", crops)
 year = st.number_input("Year", min_value=2000, max_value=2100, step=1)
 area = st.number_input("Area", min_value=0.0, step=0.1)
-
 le_state = joblib.load("le_state.pkl")
 le_district = joblib.load("le_district.pkl")
 le_season = joblib.load("le_season.pkl")
@@ -22,21 +27,24 @@ def transform_input(le, value):
     if value in le.classes_:
         return le.transform([value])[0]
     else:
-        return -1 
+        return -1
+
 state_encoded = transform_input(le_state, state)
 district_encoded = transform_input(le_district, district)
 season_encoded = transform_input(le_season, season)
 crop_encoded = transform_input(le_crop, crop)
+
 input_data = pd.DataFrame({
     'State_Name': [state_encoded],
     'District_Name': [district_encoded],
     'Season': [season_encoded],
     'Crop': [crop_encoded],
     'Area': [area],        
-    'Crop_Year': [year]    
+    'Crop_Year': [year]
 })
 input_data = input_data[feature_names]
 input_data_scaled = scaler.transform(input_data)
+
 if st.button("Predict"):
     prediction = model.predict(input_data_scaled)
     st.write(f"Predicted Crop Production: {prediction[0]}")
